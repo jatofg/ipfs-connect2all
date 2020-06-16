ed4bab98d58357e60d0fd664bacfe524be6adb65
Tobias Flaig <jan.tobias.flaig@informatik.hu-berlin.de>
2020-06-16 17:49:44 +0200

    Refactoring of analysis for reuse

---
+++ analysis/analysis.go
@@ -0,0 +1,233 @@
+package analysis
+
+import (
+	"errors"
+	"fmt"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"ipfs-connect2all/input"
+	"os"
+	"time"
+)
+
+type CrawlOrSnapshotFile struct {
+	Filename string
+	Directory string
+}
+
+type CrawlOrSnapshotFiles []CrawlOrSnapshotFile
+
+func (f CrawlOrSnapshotFile) GetPath() string {
+	return f.Directory + "/" + f.Filename
+}
+
+type FilesForAnalysis struct {
+	VisitedPeersFile *CrawlOrSnapshotFile
+	KnownPeersFile *CrawlOrSnapshotFile
+	ConnectedPeersFile *CrawlOrSnapshotFile
+	EstablishedConnectionsFile *CrawlOrSnapshotFile
+	SuccessfulConnectionsFile *CrawlOrSnapshotFile
+	FailedConnectionsFile *CrawlOrSnapshotFile
+}
+
+type MapsForAnalysis struct {
+	VisitedPeers map[peer.ID]*input.VisitedPeer
+	KnownPeers map[peer.ID]peer.ID
+	ConnectedPeers map[peer.ID]*input.ConnectedPeer
+	EstablishedConnections map[peer.ID]peer.ID
+	SuccessfulConnections map[peer.ID]peer.ID
+	FailedConnections map[peer.ID]peer.ID
+}
+
+type ComparisonResult struct {
+	DhtPeers int
+	ReachableDhtPeers int
+	DhtButNotKnown int
+	DhtButNotConnected int
+	DhtButNotSuccessful int
+	DhtButFailed int
+	KnownButNotDht int
+	ConnectedButNotDht int
+	ConnectedButNotDhtReachable int
+	SuccessfulButNotDht int
+	SuccessfulButNotDhtReachable int
+}
+
+func GetCrawlAndSnapshotFiles(crawlPath string, snapshotPath string) (CrawlOrSnapshotFiles, error) {
+	dhtCrawlDir, err := os.Open(crawlPath)
+	if err != nil {
+		return nil, fmt.Errorf("DHT crawl dir could not be opened: %s", err.Error())
+	}
+	snapshotDir, err := os.Open(snapshotPath)
+	if err != nil {
+		return nil, fmt.Errorf("Snapshot dir could not be opened: %s", err.Error())
+	}
+	dhtCrawlCandidates, err := dhtCrawlDir.Readdirnames(-1)
+	if err != nil {
+		return nil, fmt.Errorf("Contents of DHT crawl dir could not be fetched: %s", err.Error())
+	}
+	snapshotCandidates, err := snapshotDir.Readdirnames(-1)
+	if err != nil {
+		return nil, fmt.Errorf("Contents of snapshot dir could not be fetched: %s", err.Error())
+	}
+	ret := make([]CrawlOrSnapshotFile, 0, len(dhtCrawlCandidates) + len(snapshotCandidates))
+	for _, dcc := range dhtCrawlCandidates {
+		ret = append(ret, CrawlOrSnapshotFile{Filename: dcc, Directory: crawlPath})
+	}
+	for _, sc := range snapshotCandidates {
+		ret = append(ret, CrawlOrSnapshotFile{Filename: sc, Directory: snapshotPath})
+	}
+	return ret, nil
+}
+
+func (candidates CrawlOrSnapshotFiles) GetClosest(startsWith string, timestamp time.Time, dateFormat string) *CrawlOrSnapshotFile {
+	var currentDistance time.Duration = -1
+	var ret *CrawlOrSnapshotFile = nil
+	for _, currentFile := range candidates {
+		datePos := len(startsWith)
+		currentFileName := currentFile.Filename
+		if len(currentFileName) <= datePos || currentFileName[0:datePos] != startsWith {
+			continue
+		}
+		currentTimestamp, err := time.Parse(dateFormat, currentFileName[datePos:datePos+len(dateFormat)])
+		if err != nil {
+			//fmt.Printf("Could not parse date of file %s, ignoring (error: %s).", currentFileName, err.Error())
+			continue
+		}
+		duration := currentTimestamp.Sub(timestamp)
+		if duration >= 0 && (currentDistance < 0 || duration < currentDistance) {
+			ret = &CrawlOrSnapshotFile{}
+			*ret = currentFile
+			currentDistance = duration
+		}
+		if currentDistance == 0 {
+			break
+		}
+	}
+	return ret
+}
+
+func GetFilesForAnalysis(inputFiles CrawlOrSnapshotFiles, crawlTimestamp time.Time, snapshotTimestamp time.Time,
+							dateFormat string) (*FilesForAnalysis, error) {
+
+	visitedPeersFile := inputFiles.GetClosest("visitedPeers_", crawlTimestamp, dateFormat)
+	if visitedPeersFile == nil {
+		return nil, errors.New("Error: No matching DHT crawl file found.")
+	}
+
+	knownFile := inputFiles.GetClosest("known_", snapshotTimestamp, dateFormat)
+	if knownFile == nil {
+		return nil, errors.New("Error: No matching known peers snapshot file found.")
+	}
+
+	connectedFile := inputFiles.GetClosest("connected_", snapshotTimestamp, dateFormat)
+	if connectedFile == nil {
+		return nil, errors.New("Error: No matching connected peers snapshot file found.")
+	}
+
+	establishedFile := inputFiles.GetClosest("established_", snapshotTimestamp, dateFormat)
+	if establishedFile == nil {
+		return nil, errors.New("Error: No matching established connections snapshot file found.")
+	}
+
+	successfulFile := inputFiles.GetClosest("successful_", snapshotTimestamp, dateFormat)
+	if successfulFile == nil {
+		return nil, errors.New("Error: No matching successful connections snapshot file found.")
+	}
+
+	failedFile := inputFiles.GetClosest("failed_", snapshotTimestamp, dateFormat)
+	if failedFile == nil {
+		return nil, errors.New("Error: No matching failed connections snapshot file found.")
+	}
+
+	return &FilesForAnalysis{
+		VisitedPeersFile:           visitedPeersFile,
+		KnownPeersFile:             knownFile,
+		ConnectedPeersFile:         connectedFile,
+		EstablishedConnectionsFile: establishedFile,
+		SuccessfulConnectionsFile:  successfulFile,
+		FailedConnectionsFile:      failedFile,
+	}, nil
+}
+
+func GetMapsForAnalysis(filesForAnalysis FilesForAnalysis) (*MapsForAnalysis, error) {
+
+	// Peers found by DHT scan, but not by connect2all:
+	visitedPeers, err := input.LoadVisitedPeers(filesForAnalysis.VisitedPeersFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("DHT peers could not be loaded: %s", err.Error())
+	}
+	knownPeers, err := input.LoadPeerList(filesForAnalysis.KnownPeersFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("Known peers could not be loaded: %s", err.Error())
+	}
+	connectedPeers, err := input.LoadConnectedPeers(filesForAnalysis.ConnectedPeersFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("Connected peers could not be loaded: %s", err.Error())
+	}
+	successfulConnections, err := input.LoadPeerList(filesForAnalysis.SuccessfulConnectionsFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("Successful connections could not be loaded: %s", err.Error())
+	}
+	failedConnections, err := input.LoadPeerList(filesForAnalysis.FailedConnectionsFile.GetPath())
+	if err != nil {
+		return nil, fmt.Errorf("Failed connections could not be loaded: %s", err.Error())
+	}
+
+	return &MapsForAnalysis{
+		VisitedPeers: visitedPeers,
+		KnownPeers: knownPeers,
+		ConnectedPeers: connectedPeers,
+		SuccessfulConnections: successfulConnections,
+		FailedConnections: failedConnections,
+	}, nil
+
+}
+
+func CalculateComparisonResult(maps MapsForAnalysis) ComparisonResult {
+	var result ComparisonResult
+	result.DhtPeers = len(maps.VisitedPeers)
+	result.ReachableDhtPeers = result.DhtPeers
+
+	for dhtPeerID, dhtPeer := range maps.VisitedPeers {
+		if !dhtPeer.Reachable {
+			result.ReachableDhtPeers--
+			continue
+		}
+		if _, inKnown := maps.KnownPeers[dhtPeerID]; !inKnown {
+			result.DhtButNotKnown++
+		}
+		if _, inConnected := maps.ConnectedPeers[dhtPeerID]; !inConnected {
+			result.DhtButNotConnected++
+		}
+		if _, inSuccessful := maps.SuccessfulConnections[dhtPeerID]; !inSuccessful {
+			result.DhtButNotSuccessful++
+		}
+		if _, inFailed := maps.FailedConnections[dhtPeerID]; inFailed {
+			result.DhtButFailed++
+		}
+	}
+
+	for peerID := range maps.KnownPeers {
+		if _, inDht := maps.VisitedPeers[peerID]; !inDht {
+			result.KnownButNotDht++
+		}
+	}
+
+	for peerID := range maps.ConnectedPeers {
+		if _, inDht := maps.VisitedPeers[peerID]; !inDht {
+			result.ConnectedButNotDht++
+		} else if !maps.VisitedPeers[peerID].Reachable {
+			result.ConnectedButNotDhtReachable++
+		}
+	}
+
+	for peerID := range maps.SuccessfulConnections {
+		if _, inDht := maps.VisitedPeers[peerID]; !inDht {
+			result.SuccessfulButNotDht++
+		} else if !maps.VisitedPeers[peerID].Reachable {
+			result.SuccessfulButNotDhtReachable++
+		}
+	}
+
+	return result
+}
--- cmd/c2a_analysis/main.go
+++ cmd/c2a_analysis/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
+	"ipfs-connect2all/analysis"
 	"ipfs-connect2all/helpers"
-	"ipfs-connect2all/input"
-	"log"
 	"os"
 	"time"
 )
@@ -17,39 +16,30 @@ func main() {
 	configValues["Timestamp"] = ""
 	configValues["DHTCrawlDir"] = "crawls"
 	configValues["SnapshotDir"] = "snapshots"
+	configValues["SnapshotTS"] = ""
 
 	// load config from command line and display help upon encountering bad options (including -h/--help/Help/help/...)
 	if !helpers.LoadConfig(&configValues, os.Args[1:]) || configValues["Timestamp"] == "" {
 		fmt.Println("Usage: c2a_analysis [arguments]\n\n" +
 
 			"Required arguments:\n" +
-			"Timestamp=<value>         Timestamp to use (see DateFormat, next crawl and \n" +
+			"Timestamp=<value>         Timestamp to use (see DateFormat, next crawl and\n" +
 			"                          snapshots from this point will be used)\n\n" +
 
 			"Optional arguments:\n" +
 			"DateFormat=<format>       Date format (Go-style) (default: 06-01-02--15:04:05)\n" +
 
 			"DHTCrawlDir=<dir>         Directory in which the snapshots are located\n" +
-			"SnapshotDir=<dir>         Directory in which the crawl output files are located")
+			"SnapshotDir=<dir>         Directory in which the crawl output files are located\n" +
+			"SnapshotTS=<value>        Timestamp to use for snapshots (if given, the one" +
+			"                          from above will be used for the crawl only)")
 		return
 	}
 
 	// load list of candidate files
-	dhtCrawlDir, err := os.Open(configValues["DHTCrawlDir"])
+	crawlAndSnapshotFiles, err := analysis.GetCrawlAndSnapshotFiles(configValues["DHTCrawlDir"], configValues["SnapshotDir"])
 	if err != nil {
-		panic("DHT crawl dir could not be opened: " + err.Error())
-	}
-	snapshotDir, err := os.Open(configValues["SnapshotDir"])
-	if err != nil {
-		panic("Snapshot dir could not be opened: " + err.Error())
-	}
-	dhtCrawlCandidates, err := dhtCrawlDir.Readdirnames(-1)
-	if err != nil {
-		panic("Contents of DHT crawl dir could not be fetched: " + err.Error())
-	}
-	snapshotCandidates, err := snapshotDir.Readdirnames(-1)
-	if err != nil {
-		panic("Contents of snapshot dir could not be fetched: " + err.Error())
+		panic(err.Error())
 	}
 
 	// choose the closest ones
@@ -58,178 +48,50 @@ func main() {
 	if err != nil {
 		panic("Could not parse timestamp: " + err.Error())
 	}
-
-	getClosestFile := func(startsWith string, candidates []string) string {
-		var currentDistance time.Duration = -1
-		ret := ""
-		for _, currentFile := range candidates {
-			datePos := len(startsWith)
-			if len(currentFile) <= datePos || currentFile[0:datePos] != startsWith {
-				continue
-			}
-			currentTimestamp, err := time.Parse(dateFormat, currentFile[datePos:datePos+len(dateFormat)])
-			if err != nil {
-				fmt.Printf("Could not parse date of file %s, ignoring (error: %s).", currentFile, err.Error())
-				continue
-			}
-			duration := currentTimestamp.Sub(inputTimestamp)
-			if duration >= 0 && (currentDistance < 0 || duration < currentDistance) {
-				ret = currentFile
-				currentDistance = duration
-			}
-			if currentDistance == 0 {
-				break
-			}
+	var inputSnapshotTS time.Time
+	if configValues["SnapshotTS"] != "" {
+		inputSnapshotTS, err = time.Parse(dateFormat, configValues["SnapshotTS"])
+		if err != nil {
+			panic("Could not parse snapshot timestamp: " + err.Error())
 		}
-		return ret
-	}
-
-	visitedPeersFile := getClosestFile("visitedPeers_", dhtCrawlCandidates)
-	if visitedPeersFile == "" {
-		panic("Error: No matching DHT crawl file found.")
 	} else {
-		fmt.Printf("Using DHT crawl file: %s\n", visitedPeersFile)
+		inputSnapshotTS = inputTimestamp
 	}
 
-	knownFile := getClosestFile("known_", snapshotCandidates)
-	if knownFile == "" {
-		panic("Error: No matching known peers snapshot file found.")
-	} else {
-		fmt.Printf("Using known peers snapshot file: %s\n", knownFile)
+	filesForAnalysis, err := analysis.GetFilesForAnalysis(crawlAndSnapshotFiles, inputTimestamp, inputSnapshotTS,
+		dateFormat)
+	if err != nil {
+		panic(err.Error())
 	}
 
-	connectedFile := getClosestFile("connected_", snapshotCandidates)
-	if connectedFile == "" {
-		panic("Error: No matching connected peers snapshot file found.")
-	} else {
-		fmt.Printf("Using connected peers snapshot file: %s\n", connectedFile)
-	}
-
-	//establishedFile := getClosestFile("established_", snapshotCandidates)
-	//if establishedFile == "" {
-	//	panic("Error: No matching established connections snapshot file found.")
-	//} else {
-	//	fmt.Printf("Using established connections snapshot file: %s\n", establishedFile)
-	//}
-
-	successfulFile := getClosestFile("successful_", snapshotCandidates)
-	if successfulFile == "" {
-		panic("Error: No matching successful connections snapshot file found.")
-	} else {
-		fmt.Printf("Using successful connections snapshot file: %s\n", successfulFile)
-	}
-
-	failedFile := getClosestFile("failed_", snapshotCandidates)
-	if failedFile == "" {
-		panic("Error: No matching failed connections snapshot file found.")
-	} else {
-		fmt.Printf("Using failed connections snapshot file: %s\n", failedFile)
-	}
-
+	fmt.Printf("Using DHT crawl file: %s\n", filesForAnalysis.VisitedPeersFile)
+	fmt.Printf("Using known peers snapshot file: %s\n", filesForAnalysis.KnownPeersFile)
+	fmt.Printf("Using connected peers snapshot file: %s\n", filesForAnalysis.ConnectedPeersFile)
+	fmt.Printf("Using established connections snapshot file: %s\n", filesForAnalysis.EstablishedConnectionsFile)
+	fmt.Printf("Using successful connections snapshot file: %s\n", filesForAnalysis.SuccessfulConnectionsFile)
+	fmt.Printf("Using failed connections snapshot file: %s\n", filesForAnalysis.FailedConnectionsFile)
 	fmt.Println()
 
-	sWithDir := func(in string) string {
-		return configValues["SnapshotDir"] + "/" + in
-	}
-	dhtWithDir := func(in string) string {
-		return configValues["DHTCrawlDir"] + "/" + in
-	}
-
-	// Peers found by DHT scan, but not by connect2all:
-	dhtPeers, err := input.LoadVisitedPeers(dhtWithDir(visitedPeersFile))
+	mapsForAnalysis, err := analysis.GetMapsForAnalysis(*filesForAnalysis)
 	if err != nil {
-		log.Panicf("DHT peers could not be loaded: %s", err.Error())
-	}
-	knownPeers, err := input.LoadPeerList(sWithDir(knownFile))
-	if err != nil {
-		log.Panicf("Known peers could not be loaded: %s", err.Error())
-	}
-	connectedPeers, err := input.LoadConnectedPeers(sWithDir(connectedFile))
-	if err != nil {
-		log.Panicf("Connected peers could not be loaded: %s", err.Error())
-	}
-	successfulConnections, err := input.LoadPeerList(sWithDir(successfulFile))
-	if err != nil {
-		log.Panicf("Successful connections could not be loaded: %s", err.Error())
-	}
-	failedConnections, err := input.LoadPeerList(sWithDir(failedFile))
-	if err != nil {
-		log.Panicf("Failed connections could not be loaded: %s", err.Error())
+		panic(err.Error())
 	}
 
-	reachableDhtPeers := len(dhtPeers)
-	dhtButNotKnown := 0
-	dhtButNotConnected := 0
-	dhtButNotSuccessful := 0
-	dhtButFailed := 0
-	for dhtPeerID, dhtPeer := range dhtPeers {
-		if !dhtPeer.Reachable {
-			reachableDhtPeers--
-			continue
-		}
-		if _, inKnown := knownPeers[dhtPeerID]; !inKnown {
-			dhtButNotKnown++
-		}
-		if _, inConnected := connectedPeers[dhtPeerID]; !inConnected {
-			dhtButNotConnected++
-		}
-		if _, inSuccessful := successfulConnections[dhtPeerID]; !inSuccessful {
-			dhtButNotSuccessful++
-		}
-		if _, inFailed := failedConnections[dhtPeerID]; inFailed {
-			dhtButFailed++
-		}
-	}
+	comparisonResult := analysis.CalculateComparisonResult(*mapsForAnalysis)
 
-	knownButNotDht := 0
-	for peerID := range knownPeers {
-		if _, inDht := dhtPeers[peerID]; !inDht {
-			knownButNotDht++
-		}
-	}
+	fmt.Printf("Reachable DHT peers: %d (total: %d)\n", comparisonResult.ReachableDhtPeers, comparisonResult.DhtPeers)
+	fmt.Printf("Peers known: %d; connected: %d\n", len(mapsForAnalysis.KnownPeers), len(mapsForAnalysis.ConnectedPeers))
+	fmt.Printf("Connections successful: %d; failed: %d\n\n", len(mapsForAnalysis.SuccessfulConnections), len(mapsForAnalysis.FailedConnections))
 
-	connectedButNotDht := 0
-	connectedButNotDhtReachable :=0
-	for peerID := range connectedPeers {
-		if _, inDht := dhtPeers[peerID]; !inDht {
-			connectedButNotDht++
-		} else if !dhtPeers[peerID].Reachable {
-			connectedButNotDhtReachable++
-		}
-	}
-
-	successfulButNotDht := 0
-	successfulButNotDhtReachable :=0
-	for peerID := range successfulConnections {
-		if _, inDht := dhtPeers[peerID]; !inDht {
-			successfulButNotDht++
-		} else if !dhtPeers[peerID].Reachable {
-			successfulButNotDhtReachable++
-		}
-	}
-
-	failedButDhtReachable := 0
-	for peerID := range failedConnections {
-		if _, inDht := dhtPeers[peerID]; inDht && dhtPeers[peerID].Reachable {
-			failedButDhtReachable++
-		}
-	}
-
-
-	fmt.Printf("Reachable DHT peers: %d (total: %d)\n", reachableDhtPeers, len(dhtPeers))
-	fmt.Printf("Peers known: %d; connected: %d\n", len(knownPeers), len(connectedPeers))
-	fmt.Printf("Connections successful: %d; failed: %d\n\n", len(successfulConnections), len(failedConnections))
-
-	fmt.Printf("DHT-reachable, but not c2a-known: %d\n", dhtButNotKnown)
-	fmt.Printf("DHT-reachable, but not c2a-connected: %d\n", dhtButNotConnected)
-	fmt.Printf("DHT-reachable, but not c2a-successful: %d\n", dhtButNotSuccessful)
-	fmt.Printf("DHT-reachable, but c2a-failed: %d\n", dhtButFailed)
-	fmt.Printf("c2a-known, but not in DHT crawl: %d\n", knownButNotDht)
-	fmt.Printf("c2a-connected, but not in DHT crawl: %d\n", connectedButNotDht)
-	fmt.Printf("c2a-connected, but not marked as reachable in DHT crawl: %d\n", connectedButNotDhtReachable)
-	fmt.Printf("c2a-successful, but not in DHT crawl: %d\n", successfulButNotDht)
-	fmt.Printf("c2a-successful, but not DHT-reachable: %d\n", successfulButNotDhtReachable)
-	fmt.Printf("c2a-failed, but DHT-reachable: %d\n", failedButDhtReachable)
+	fmt.Printf("DHT-reachable, but not c2a-known: %d\n", comparisonResult.DhtButNotKnown)
+	fmt.Printf("DHT-reachable, but not c2a-connected: %d\n", comparisonResult.DhtButNotConnected)
+	fmt.Printf("DHT-reachable, but not c2a-successful: %d\n", comparisonResult.DhtButNotSuccessful)
+	fmt.Printf("DHT-reachable, but c2a-failed: %d\n", comparisonResult.DhtButFailed)
+	fmt.Printf("c2a-known, but not in DHT crawl: %d\n", comparisonResult.KnownButNotDht)
+	fmt.Printf("c2a-connected, but not in DHT crawl: %d\n", comparisonResult.ConnectedButNotDht)
+	fmt.Printf("c2a-connected, but not marked as reachable in DHT crawl: %d\n", comparisonResult.ConnectedButNotDhtReachable)
+	fmt.Printf("c2a-successful, but not in DHT crawl: %d\n", comparisonResult.SuccessfulButNotDht)
+	fmt.Printf("c2a-successful, but not DHT-reachable: %d\n", comparisonResult.SuccessfulButNotDhtReachable)
 	// neither known nor failed?
 
 
